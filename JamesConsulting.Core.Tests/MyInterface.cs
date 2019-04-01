d3f99150d29a0e7b900b43cca846274ab4496665
Rudy James <rudy@jamesconsulting.biz>
2019-04-01 13:02:07 -0500

    Nuget package update

--- JamesConsulting.Core.Tests/MyInterface.cs
+++ JamesConsulting.Core.Tests/MyInterface.cs
@@ -13,6 +13,8 @@ namespace JamesConsulting.Core.Tests
     using System;
     using System.Threading.Tasks;
 
+    using Xunit;
+
     /// <summary>
     ///     The my interface.
     /// </summary>
@@ -23,6 +25,34 @@ namespace JamesConsulting.Core.Tests
         {
             await Task.Delay(100).ConfigureAwait(false);
             return new MyClass { X = id, Y = id.ToString() };
+        }
+
+        /// <inheritdoc />
+        public Task<T> GetClassById<T>(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// The get class by id.
+        /// </summary>
+        /// <param name="id">
+        /// The id.
+        /// </param>
+        /// <typeparam name="T">
+        /// </typeparam>
+        /// <typeparam name="K">
+        /// </typeparam>
+        /// <returns>
+        /// The <see cref="Task"/>.
+        /// </returns>
+        /// <exception cref="NotImplementedException">
+        /// </exception>
+        public Task<K> GetClassById<T, K>(int id)
+            where K : class where T : class
+        {
+            GetClassById<T>(id);
+            return default(Task<K>);
         }
 
         /// <inheritdoc />
