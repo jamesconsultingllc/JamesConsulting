2078486c0b716b39be060393c1a0933404d63c03
Rudy James <rudy@jamesconsulting.biz>
2022-08-13 11:01:41 -0500

    Added .Net 6 Support and added StreamExtensions.cs Deserialize method

--- JamesConsulting/IO/StreamExtensions.cs
+++ JamesConsulting/IO/StreamExtensions.cs
@@ -11,6 +11,7 @@
 using System;
 using System.IO;
 using System.Text;
+using Newtonsoft.Json;
 using PostSharp.Patterns.Contracts;
 
 namespace JamesConsulting.IO
@@ -36,6 +37,15 @@ namespace JamesConsulting.IO
             stream.Position = 0;
             stream.Read(firstBytes, 0, 2);
             return Encoding.UTF8.GetString(firstBytes) == "MZ";
+        }
+
+        public static T Deserialize<T>([NotNull] this Stream stream)
+        {
+            if (stream == null) throw new ArgumentNullException(nameof(stream));
+            using var sr = new StreamReader(stream);
+            using JsonReader reader = new JsonTextReader(sr);
+            var serializer = new JsonSerializer();
+            return serializer.Deserialize<T>(reader);
         }
     }
 }
